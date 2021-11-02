a87ac2e9df6940bd0eefdb4b79098f10811d9798
juliocezar96 <juliocr.jc38@gmail.com>
2021-11-01 23:21:00 -0300

    implementação da model e e controller

---
+++ src/main/java/ibm/blue/bank/blu/bank/controller/ClienteController.java
@@ -0,0 +1,19 @@
+package ibm.blue.bank.blu.bank.controller;
+
+import ibm.blue.bank.blu.bank.model.Cliente;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value ="/cliente")
+public class ClienteController {
+
+    //endPOint
+    @GetMapping
+    public ResponseEntity<Cliente> findAll(){
+        Cliente cliente = new Cliente(1L,"Julio","81999999999","julio@teste.com","11122233301");
+        return ResponseEntity.ok().body(cliente);
+    }
+}
---
+++ src/main/java/ibm/blue/bank/blu/bank/model/Cliente.java
@@ -0,0 +1,69 @@
+package ibm.blue.bank.blu.bank.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+
+@EqualsAndHashCode
+public class Cliente implements Serializable {
+
+    private static  final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String cpf;
+
+    public Cliente(){}
+
+    public Cliente(Long id, String nome, String telefone, String email, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
